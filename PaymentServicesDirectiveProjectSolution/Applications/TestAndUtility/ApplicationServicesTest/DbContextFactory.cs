12b6e554c6a16c9f060b0e43a29c3593d13036f2
dmacura <dmacura@smartcodehouse.com>
2021-04-14 14:37:37 +0200

    Added tests.

---
+++ PaymentServicesDirectiveProjectSolution/Applications/TestAndUtility/ApplicationServicesTest/DbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Infrastructure.DataAccess.EFDataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Applications.TestAndUtility.ApplicationServicesTest
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<PSDDbContext>
+    {
+        public PSDDbContext CreateDbContext(string[] args)
+        {
+            EfConnectionSettings.ConfigureSqlServerConnection(@"Data Source=localhost;Initial Catalog=PSD;user id=sa;password=Beograd011!");
+
+            return new PSDDbContext();
+        }
+    }
+}
---
+++ PaymentServicesDirectiveProjectSolution/Applications/TestAndUtility/ApplicationServicesTest/UserServiceTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationService;
+using Banks.ApplicationServiceInterfaces;
+using Banks.ApplicationServices;
+using Domain.ApplicationService;
+using Domain.DTOs;
+using Domain.Repositories;
+using Infrastructure.DataAccess.EFDataAccess;
+using Infrastructure.DataAccess.EFDataAccess.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Applications.TestAndUtility.ApplicationServicesTest
+{
+    [TestClass]
+    public class UserServiceTest
+    {
+        private PSDDbContext _context;
+        private UserService _userService;
+        private IUserRepository _userRepository;
+        private IUnitOfWork _unitOfWork;
+        private IUnitOfWorkFactory _unitOfWorkFactory;
+        private IBankServiceProvider _bankServiceProvider;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            DbContextFactory dbContextFactory = new DbContextFactory();
+            _context = dbContextFactory.CreateDbContext(new string[] { });
+            _unitOfWorkFactory = new EfUnitOfWorkFactory();
+            _unitOfWork = _unitOfWorkFactory.CreateUnitOfWork();
+            _userRepository = new EFUserRepository(_context);
+            _bankServiceProvider = new BankServiceProvider();
+            _bankServiceProvider.Add("dummy", new DummyBankService());
+            _userService = new UserService(_userRepository,_unitOfWork,_unitOfWorkFactory, _bankServiceProvider);
+
+        }
+
+        [TestCleanup()]
+        public async Task Cleanup()
+        {
+            await _context.DisposeAsync();
+            _unitOfWork = null;
+        }
+
+        [TestMethod]
+        public async Task TestCreateCorrectUser()
+        {
+            try
+            {
+                UserDto user = await _userService.CreateUser("Dragan", "Macura", "0312984710064", "dummy", "160-9999-00",
+                        "1234");
+
+                Assert.AreNotEqual(null, user, "User must not be null");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("Unexpected error: " + ex.Message);
+            }
+        }
+    }
+}
